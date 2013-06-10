dd902273b91f3e8ccdd649bade77912b9fa81eeb
thks10 <softinus@users.noreply.github.com>
2013-06-10 01:00:09 +0000

    smartcurator 새버전

---
+++ SmartCurator5/src/com/raimsoft/smartcurator5/MainApplication.java
@@ -0,0 +1,27 @@
+package com.raimsoft.smartcurator5;
+
+import com.kth.baasio.Baas;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void onCreate()
+	{
+		Baas.io().init(
+				this
+				,"https://api.baas.io"
+				,"a769dd7d-4a53-11e2-b5a4-06ebb80000ba"
+				,"977706bf-d02a-11e2-9d52-06f4fe0000b5");
+		super.onCreate();
+	}
+
+	@Override
+	public void onTerminate()
+	{
+		Baas.io().uninit(this);
+		super.onTerminate();
+	}
+
+}
