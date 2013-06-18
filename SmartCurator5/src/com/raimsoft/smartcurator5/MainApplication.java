371dd61a69b622b66f9458fdd4d0cb07644b135f
thks10 <softinus@users.noreply.github.com>
2013-06-18 16:44:35 +0000

    baas.io 대신 Parse 써봤음.

--- SmartCurator5/src/com/raimsoft/smartcurator5/MainApplication.java
+++ SmartCurator5/src/com/raimsoft/smartcurator5/MainApplication.java
@@ -2,26 +2,23 @@ package com.raimsoft.smartcurator5;
 
 import android.app.Application;
 
-import com.kth.baasio.Baas;
-
 public class MainApplication extends Application {
 
-	@Override
-	public void onCreate()
-	{
-		Baas.io().init(
-				this
-				,"https://api.baas.io"
-				,"a769dd7d-4a53-11e2-b5a4-06ebb80000ba"
-				,"977706bf-d02a-11e2-9d52-06f4fe0000b5");
-		super.onCreate();
-	}
-
-	@Override
-	public void onTerminate()
-	{
-		Baas.io().uninit(this);
-		super.onTerminate();
-	}
+//	@Override
+//	public void onCreate()
+//	{
+//        Baas.io().init(this,
+//        		"https://api.baas.io",
+//        		"june",
+//        		"smartcurator");
+//		super.onCreate();
+//	}
+//
+//	@Override
+//	public void onTerminate()
+//	{
+//		Baas.io().uninit(this);
+//		super.onTerminate();
+//	}
 
 }
