badecf8d7da5b7da4626a1354b12310562954f48
thks10 <thks10@f93d49b2-0e7a-4222-9757-86b36a3d9f92>
2013-06-19 14:20:17 +0000

    NFC 등록 리스트 업. form 전체 UI 완성. 파일 업로드 기능.

--- trunk/NFCProject/NFCProject/MainWindow.xaml.cs
+++ trunk/NFCProject/NFCProject/MainWindow.xaml.cs
@@ -14,12 +14,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Parse;
+using Microsoft.Win32;
+using System.IO;
 
-namespace WPFParseStarterProject {
+namespace NFCProject {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class MainWindow : Window {
+  public partial class MainWindow : Window
+  {
+      List<ParseObject> m_ListObject;
+      BitmapImage m_bitmap1, m_bitmap2;
+
     public MainWindow()
     {
       InitializeComponent();
@@ -34,6 +40,8 @@ namespace WPFParseStarterProject {
     {
         ParseAnalytics.TrackAppOpenedAsync();
 
+        m_ListObject = new List<ParseObject>();
+
         //var testObject = new ParseObject("Windows");
         //testObject["WPF"] = "okay";
         //await testObject.SaveAsync();
@@ -45,9 +53,13 @@ namespace WPFParseStarterProject {
 
     private async Task RefreshList()
     {
-        int nCount= lst_Recent.Items.Count;
-        for (int i = 0; i < nCount; ++i)
+        int nCount1= lst_Recent.Items.Count;
+        for (int i = 0; i < nCount1; ++i)
             lst_Recent.Items.RemoveAt(0);
+
+        int nCount2 = lst_Regist.Items.Count;
+        for (int i = 0; i < nCount2; ++i)
+            lst_Regist.Items.RemoveAt(0);
 
         var query = ParseObject.GetQuery("NFC_List").WhereEqualTo("linked_user", "june");
         IEnumerable<ParseObject> results = await query.FindAsync();
@@ -56,7 +68,19 @@ namespace WPFParseStarterProject {
         while (e1.MoveNext())
         {
             ParseObject obj = (ParseObject)(e1.Current);
+            m_ListObject.Add(obj);
             lst_Recent.Items.Add(obj.Get<string>("NFC_id"));
+        }
+
+        var query2 = ParseObject.GetQuery("NFC_reg").WhereEqualTo("linked_user", "june");
+        IEnumerable<ParseObject> results2 = await query2.FindAsync();
+
+        IEnumerator e2 = results2.GetEnumerator();
+        while (e2.MoveNext())
+        {
+            ParseObject obj = (ParseObject)(e2.Current);
+            //m_ListObject.Add(obj);
+            lst_Regist.Items.Add(obj.Get<string>("NFC_id"));
         }
     }
 
@@ -64,5 +88,107 @@ namespace WPFParseStarterProject {
     {
         await RefreshList();
     }
+
+      // 선택이 바뀌면
+    private void lst_Recent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        ParseObject obj = m_ListObject[lst_Recent.SelectedIndex];
+        
+        DateTime? timeCreated = obj.CreatedAt;
+        txt_date.Text= timeCreated.ToString();
+
+        txt_id.Text= obj.Get<string>("NFC_id");
+
+    }
+
+      // 등록
+    private async void btn_regist_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (txt_caption1.Text.Equals(""))
+        {
+            MessageBoxResult result = MessageBox.Show("Please input caption.");
+            return;
+        }
+        else if (txt_image1.Text.Equals("") || txt_image2.Text.Equals(""))
+        {
+            MessageBoxResult result = MessageBox.Show("Please select images.");
+            return;
+        }
+
+        byte[] data1 = this.ConvertImageToByte(m_bitmap1);
+        ParseFile file1 = new ParseFile("1.png", data1);
+        await file1.SaveAsync();
+
+        byte[] data2 = this.ConvertImageToByte(m_bitmap2);
+        ParseFile file2 = new ParseFile("2.png", data2);
+        await file2.SaveAsync();
+
+        var NFCreg = new ParseObject("NFC_reg");
+        NFCreg["NFC_id"] = txt_id.Text;
+        NFCreg["linked_user"] = "june";
+        NFCreg["Caption"] = txt_caption1.Text;
+        NFCreg["File1"] = file1;
+        NFCreg["File2"] = file2;
+        await NFCreg.SaveAsync();
+
+        MessageBox.Show("Regist finish!");
+    }
+
+    private void btn_remove_Click_1(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+
+    private void SetImage(int nNumber)
+    {
+        OpenFileDialog openDialog = new OpenFileDialog();
+        if (openDialog.ShowDialog() == true)
+        {
+            if (File.Exists(openDialog.FileName))
+            {
+                BitmapImage bitmapImage = new BitmapImage(new Uri(openDialog.FileName, UriKind.RelativeOrAbsolute));
+
+                if (nNumber == 1)
+                {
+                    m_bitmap1 = bitmapImage;
+                    img_1.Source = bitmapImage;
+                    txt_image1.Text = openDialog.FileName;
+                }
+                if (nNumber == 2)
+                {
+                    m_bitmap2 = bitmapImage;
+                    img_2.Source = bitmapImage;
+                    txt_image2.Text = openDialog.FileName;
+                }
+
+            }
+        }
+    }
+
+    private void btn_image1_Click(object sender, RoutedEventArgs e)
+    {
+        this.SetImage(1);
+    }
+
+    private void btn_image2_Click(object sender, RoutedEventArgs e)
+    {
+        this.SetImage(2);
+    }
+
+    private byte[] ConvertImageToByte(BitmapImage bitmap)
+    {
+        byte[] data;
+        JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(bitmap));
+        using (MemoryStream ms = new MemoryStream())
+        {
+            encoder.Save(ms);
+            data = ms.ToArray();
+        }
+
+        return data;
+    }
+
   }
 }
