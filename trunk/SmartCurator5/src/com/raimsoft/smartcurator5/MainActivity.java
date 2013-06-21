6ae78246ee39f1a4a7c648825c8d5708775c6502
thks10 <thks10@f93d49b2-0e7a-4222-9757-86b36a3d9f92>
2013-06-21 10:45:35 +0000

    -이미지 불러오기 -이미지 로딩시 프로그레스

--- trunk/SmartCurator5/src/com/raimsoft/smartcurator5/MainActivity.java
+++ trunk/SmartCurator5/src/com/raimsoft/smartcurator5/MainActivity.java
@@ -1,17 +1,28 @@
 package com.raimsoft.smartcurator5;
 
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-import com.parse.ParseObject;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ProgressCallback;
 
 
 public class MainActivity extends Activity 
@@ -19,7 +30,68 @@ public class MainActivity extends Activity
 	private NfcAdapter nfcAdapter;
 	private PendingIntent pendingIntent;
 	
-	private TextView TXT_tagDesc;
+	private TextView TXT_tagDesc, TXT_1, TXT_2;
+	private ImageView IMG_1, IMG_2;
+
+	private void GetNFCTag()
+	{
+		Tag tag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		if (tag == null)
+			return;
+		
+		
+		byte[] tagId = tag.getId();
+		String strNFC_id= toHexString(tagId);	
+		TXT_tagDesc.setText("TagID: " + strNFC_id );
+	
+		ParseObject testObject = new ParseObject("NFC_List");
+		testObject.put("NFC_id", strNFC_id);
+		testObject.put("linked_user", "june");
+		testObject.saveInBackground();
+		
+		GetImage(IMG_1, strNFC_id, "File1");
+		GetImage(IMG_2, strNFC_id, "File2");
+	}
+
+	private void GetImage(final ImageView view, final String strNFC_id, final String strFileColumnName)
+	{
+		ParseQuery<ParseObject> query= ParseQuery.getQuery("NFC_reg");
+		query.whereEqualTo("NFC_id", strNFC_id);
+		query.findInBackground(new FindCallback<ParseObject>()
+		{			
+			@Override
+			public void done(List<ParseObject> list, ParseException e)
+			{
+				if(list.size()==1)
+				{
+					ParseFile fileImage1= (ParseFile)list.get(0).get(strFileColumnName);
+					fileImage1.getDataInBackground(new GetDataCallback()
+					{	
+						@Override
+						public void done(byte[] file, ParseException e)
+						{
+							BitmapFactory.Options opts = new BitmapFactory.Options();
+							opts.inDither= false;
+							opts.inSampleSize= 2;
+							Bitmap bitmap= BitmapFactory.decodeByteArray(file, 0, file.length, opts);
+							view.setImageBitmap(bitmap);
+						}
+					},
+					new ProgressCallback()
+					{	
+						@Override
+						public void done(Integer nProgress)
+						{
+							if(nProgress==100)
+								TXT_1.setText("");
+							else
+								TXT_1.setText(Integer.toString(nProgress)+"%");
+						}
+					});
+				}
+			}
+		});
+	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -28,6 +100,10 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
         
         TXT_tagDesc = (TextView)findViewById(R.id.txt_tag);
+        IMG_1= (ImageView) findViewById(R.id.imageView1);
+        IMG_2= (ImageView) findViewById(R.id.imageView2);
+        TXT_1= (TextView) findViewById(R.id.textView1);
+        TXT_2= (TextView) findViewById(R.id.textView2);
         
 		Parse.initialize(this, "vh60sQDbtfnIlFxn5HrK6oBj5SN1rqYeqtixIngY", "23gt0kLNMnZJEPYYYxWXQFVZiRXMceKrU1Ge3kVz"); 
 		ParseAnalytics.trackAppOpened(getIntent());
@@ -38,19 +114,10 @@ public class MainActivity extends Activity
         Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         
-        Tag tag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-		if (tag != null) 
-		{
-			byte[] tagId = tag.getId();
-			TXT_tagDesc.setText("TagID: " + toHexString(tagId));
-		
-			ParseObject testObject = new ParseObject("NFC_List");
-			testObject.put("NFC_id", toHexString(tagId));
-			testObject.put("linked_user", "june");
-			testObject.saveInBackground();	
-
-		}
+        GetNFCTag();
     }
+
+
 
 
 	@Override
@@ -58,12 +125,7 @@ public class MainActivity extends Activity
 	{
 		super.onNewIntent(intent);
 		
-		Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-		if (tag != null) 
-		{
-			byte[] tagId = tag.getId();
-			TXT_tagDesc.setText("TagID: " + toHexString(tagId));
-		}
+		GetNFCTag();
 	}
 	
 	
